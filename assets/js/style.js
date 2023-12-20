38ee489254811299db0ae67990253c035d9cef42
cpayment <cpayment12@gmail.com>
2023-12-20 14:00:43 -0600

    Added Date, saving past search buttons now work properly, displays temperature in impreial correctly

--- assets/js/style.js
+++ assets/js/style.js
@@ -13,6 +13,15 @@ var windEl = document.getElementById('wind');
 // Grab the Humidity element
 var humidityEl = document.getElementById('humidity');
 
+// Save the Date Object into a var called date
+var date = new Date();
+// Save the current Month into a var called month
+var month = date.getMonth() + 1;
+// Save the current Day of the Month into a var called month
+var day = date.getDate();
+// Save the current Year into a var called month
+var year = date.getFullYear();
+
 // When the User clicks the Search Button
 searchButtonEl.addEventListener('click', function(event) {
     // CREATE a NEW var and store the Users Input
@@ -22,7 +31,7 @@ searchButtonEl.addEventListener('click', function(event) {
     // Store the Users Input into a City var
     var city = userInputValue;
     // Create the URL with the given parameters
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey + '&units=imperial';
     // FETCH the Weather API with the queryURL
     fetch(queryURL)
         .then(function(response){
@@ -35,6 +44,8 @@ searchButtonEl.addEventListener('click', function(event) {
             // IF the Input Field is BLANK or CITY NOT FOUND
             if (data.cod === '400' || data.cod === '404')
             {
+                // Clear the Input Field
+                userInputEl.value = '';
                 // LOG to Console "Error"
                 console.log('Error');
             }
@@ -53,18 +64,59 @@ searchButtonEl.addEventListener('click', function(event) {
                 cityButtonEl.innerHTML = userInputValue;
 
                 // Clear the Input Field
-                
-
-                // Clear whats in the current Display Field
+                userInputEl.value = '';
+                // Clear the City Name Field
                 cityNameEl.innerHTML = '';
+                // Clear the Temperature Field
                 temperatureEl.innerHTML = '';
+                // Clear the Wind Field
                 windEl.innerHTML = '';
+                // Clear the Humidity Field
                 humidityEl.innerHTML = '';
 
-                cityNameEl.innerHTML = data.name;
+                // Insert NEW City Name and Date
+                cityNameEl.innerHTML = data.name + ' (' + month + '/' + day + '/' + year + ')';
+                // Insert NEW Temperature data
                 temperatureEl.innerHTML = 'Temp: ' + temperatureEl.textContent + data.main.temp + '°F';
+                // Insert NEW Wind data
                 windEl.innerHTML = 'Wind: ' + windEl.textContent + data.wind.speed + ' MPH';
+                // Insert NEW Humidity data
                 humidityEl.innerHTML = 'Humidity: ' + humidityEl.textContent + data.main.humidity +' %';
             }
+
+            // When the User CLICKS any City Button they previously searched
+            cityButtonEl.addEventListener('click', function(event) {
+                // Use the Current Name on the Button and Save it to the City var
+                city = cityButtonEl.innerHTML;
+                // FETCH the Weather API with the queryURL
+                fetch(queryURL)
+                    .then(function(response){
+                        return response.json();
+                    })
+                    .then(function(data){
+                        // LOG Data to console
+                        console.log(data);
+
+                        // Clear the Input Field
+                        userInputEl.value = '';
+                        // Clear the City Name Field
+                        cityNameEl.innerHTML = '';
+                        // Clear the Temperature Field
+                        temperatureEl.innerHTML = '';
+                        // Clear the Wind Field
+                        windEl.innerHTML = '';
+                        // Clear the Humidity Field
+                        humidityEl.innerHTML = '';
+
+                        // Insert NEW City Name and Date
+                        cityNameEl.innerHTML = data.name + ' (' + month + '/' + day + '/' + year + ')';
+                        // Insert NEW Temperature data
+                        temperatureEl.innerHTML = 'Temp: ' + temperatureEl.textContent + data.main.temp + '°F';
+                        // Insert NEW Wind data
+                        windEl.innerHTML = 'Wind: ' + windEl.textContent + data.wind.speed + ' MPH';
+                        // Insert NEW Humidity data
+                        humidityEl.innerHTML = 'Humidity: ' + humidityEl.textContent + data.main.humidity +' %';
+                    });
+            })
         });
-})
+    })
